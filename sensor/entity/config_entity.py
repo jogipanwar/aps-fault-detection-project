960c3f7336871f262c27a905bc3d040cd25700a7
Jogender SIngh <jogipanwar@gmail.com>
2022-12-30 00:28:51 +0530

    Data Ingestion Started

--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -1,7 +1,20 @@
- 
-class DataIngestionConfig:...
+import os 
+
+class TrainingPipelineConfig:
+    def __init__(self):
+        self.artifact_dir = os.path.join(os.getcwd(),"artifact",f"{datetime.now().strftime('%m%d%y__%H%M%S')}")
+
+
+# class DataIngestionConfig:... three dot represent pass
+
+class DataIngestionConfig:
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.database_name = "aps"
+        self.collection_name = "sensor"
+        self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir, "data_ingestion")
+
 class DataValidationConfig:...
-class DataTransformationConfig:...
+class DataTransformationConfig:...  
 class ModelTrainerConfig:...
 class ModelEvaluationConfig:...
 class ModelPusherConfig:...
---
+++ sensor/utils/__init__.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sensor.logger import logging
+from sensor.exception import SensorException
+from sensor.config import mongo_client
+def get_collection_as_dataframe(database_name:str,collection_name:str)->pd.DataFrame:
+    try:
+        logging,info(f"Reading Data from Database: {database_name} and collection: {collection_name}")
+        df =  pd.DataFrame(list(mongo_client[database_name][collection_name].find()))
+        logging.info(f"Found columns:" {df.columns})
+        if "__id" in df.columns:
+            df = df.drop("__id",axis=1)
+        logging.info(f"Row and columns")
+        return df
+
+    except Exception as e:
+        raise SensorException(e, sys)
+
+if __name__ == "__main__"
